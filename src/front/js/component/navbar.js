3cb62bdf34292c0832c159fb4835fe49221bef95
GustavoColombia <157833803+GustavoColombia@users.noreply.github.com>
2024-08-02 18:53:48 -0500

    actualización

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,63 +2,52 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/index.css";
 import beer from "../../img/beer.png";
-import Login from "../pages/login.jsx";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg nav mb-5">
-      <div className="container-fluid ">
-        <Link to={"/"}>HOME</Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarTogglerDemo01"
-          aria-controls="navbarTogglerDemo01"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-5">
-          <div>
-            <li className="nav-item mx-3">
-              <a 
-                className="nav-link active mx-2" 
-                aria-current="page" 
-                href="#" 
-              
-              >
-                <Link to={"/"}  style={{ color: '#4B2E1D' }}>HOME</Link>
-                
-              </a>
-            </li>
-            <li className="nav-item">
-              <Link
-                className="navbar-brand d-flex justify-content-center my-2 ms-5 "
-                to="#"
-              >
-                CERVECERIA
-              </Link>
-            </li>
-            <div className="mx-5 ms-5">
-              <img
-                src={beer}
-                alt="BEER"
-                width="50"
-                height="50"
-                className="mx-5"
-              />
-            </div>
-          </ul>
+    <nav
+      className="navbar navbar-expand-lg nav mb-5"
+      style={{ backgroundColor: "#FFD479" }}
+    >
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        <div className="d-flex">
+          <Link
+            to="/"
+            className="nav-link mx-3"
+            style={{ color: "#4B2E1D", textDecoration: "underline" }}
+          >
+            HOME
+          </Link>
+          <Link to="/" className="nav-link mx-3" style={{ color: "#4B2E1D" }}>
+            ESTILOS
+          </Link>
+          <Link to="/" className="nav-link mx-3" style={{ color: "#4B2E1D" }}>
+            CERVECERIA
+          </Link>
+        </div>
+        <div className="text-center">
+          <img
+            src={beer}
+            alt="BEER"
+            width="80"
+            height="80"
+            className="rounded-circle"
+          />
+        </div>
+        <div className="d-flex align-items-center">
           <form className="d-flex me-3" role="search">
             <input
               className="form-control me-2"
               type="search"
               placeholder="Search"
               aria-label="Search"
+              style={{ width: "150px" }}
             />
-            <button className="btn btn-outline-success" type="submit">
+            <button
+              className="btn btn-outline d-flex align-items-center justify-content-center"
+              style={{ color: "#4B2E1D" }}
+              type="submit"
+            >
               SOBRE NOSOTROS
             </button>
           </form>
@@ -69,18 +58,19 @@ export const Navbar = () => {
               data-bs-toggle="dropdown"
               aria-expanded="false"
             >
-              <span className="visually-hidden">Toggle Dropdown</span>
               <i className="fa-solid fa-users"></i>
             </button>
             <ul className="dropdown-menu dropdown-menu-end my-4">
               <li>
-                <Link className="dropdown-item" to="">
+                <Link className="dropdown-item" to="#">
                   Favoritos
                   <i className="fa-regular fa-heart mx-2"></i>
                 </Link>
               </li>
               <li>
-                <Link to={"/login"}> Iniciar Sesión</Link>
+                <Link to="/login" className="dropdown-item">
+                  Iniciar Sesión
+                </Link>
               </li>
               <li>
                 <hr className="dropdown-divider" />
