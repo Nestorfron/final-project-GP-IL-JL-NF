5dfc3538c575137d46ce7b8aea965a55d146c906
J-LopezE <jorgeisrael.lopez@gmail.com>
2024-08-03 13:37:14 -0600

    agregue funcion getStyle y renderizado en nav

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,8 @@
 const getState = ({ getStore, getActions, setStore }) => {
   return {
-    store: {},
+    store: {
+      styles: [],
+    },
     actions: {
       register: async (email, password, is_brewer, country) => {
         try {
@@ -48,43 +50,13 @@ const getState = ({ getStore, getActions, setStore }) => {
       logout: () => {
         localStorage.removeItem("token");
       },
-      add_brewery: async (
-        name,
-        address,
-        history,
-        facebook_url,
-        instagram_url,
-        picture_of_brewery_url,
-        x_url,
-        logo_of_brewery_url
-      ) => {
-        const jwt = localStorage.getItem("token");
+      getStyles: async () => {
         try {
-          const response = await fetch(
-            process.env.BACKEND_URL + "/api/create_new_brewery",
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-                authorization: `Bearer ${jwt}`,
-              },
-              body: JSON.stringify({
-                name,
-                address,
-                history,
-                facebook_url,
-                instagram_url,
-                picture_of_brewery_url,
-                x_url,
-                logo_of_brewery_url,
-              }),
-            }
-          );
-          if (!response.ok) {
-            return false;
-          }
+          const response = await fetch(process.env.BACKEND_URL + "/api/styles");
           const data = await response.json();
-          return data;
+          if (response.ok) {
+            setStore({ styles: data });
+          }
         } catch (error) {
           console.log(error);
         }
