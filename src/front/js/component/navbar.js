47a76951800edaacf2ffc9410ddbe1ac29258cc0
Iván Loza <ivanloza@Ivans-Laptop.local>
2024-08-06 18:49:50 -0500

    Width of home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -25,7 +25,7 @@ export const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
+      <div className="container-fluid w-75">
         <button
           className="navbar-toggler"
           type="button"
