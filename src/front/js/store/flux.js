d2d3e7d8bce3458069d9e7a75219303ea4f3cdb4
J-LopezE <jorgeisrael.lopez@gmail.com>
2024-08-13 21:35:44 -0600

    flux

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -9,6 +9,7 @@ const getState = ({ getStore, getActions, setStore }) => {
       userBeers: [],
       breweryEvents: [],
       userStyles: [],
+      LatLng: [],
     },
     actions: {
       //REGISTER USER//
@@ -229,8 +230,6 @@ const getState = ({ getStore, getActions, setStore }) => {
             }
           );
           if (!response.ok) {
-            const errorText = await response.text();
-            console.log("Error:", errorText);
             return false;
           }
           const data = await response.json();
@@ -528,26 +527,6 @@ const getState = ({ getStore, getActions, setStore }) => {
           }
           const data = await response.json();
           return data;
-        } catch (error) {
-          console.log(error);
-        }
-      },
-      getBeerDetails: async (beer_id) => {
-        try {
-          const response = await fetch(
-            process.env.BACKEND_URL + `/api/beer/${beer_id}`,
-            {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          );
-          if (!response.ok) {
-            return false;
-          }
-          const data = await response.json();
-          setStore({ beerDetails: data });
         } catch (error) {
           console.log(error);
         }
