5083bd4a5dfe9e9125497bf07f97ea7b80dfea18
Nestorfron <nestorfrones07@hotmail.com>
2024-08-15 22:19:05 -0300

    fusion de ramas

---
+++ migrations/versions/f682fb08ec53_fusión_de_ramas_de_migración.py
@@ -0,0 +1,24 @@
+"""Fusión de ramas de migración
+
+Revision ID: f682fb08ec53
+Revises: 
+Create Date: 2024-08-15 22:14:09.204585
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f682fb08ec53'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
